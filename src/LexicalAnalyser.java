05c9e41783ac1f5ba9dca71c462f75d402013b1e
Rosemary <13196225@student.uts.edu.au>
2019-10-01 14:50:17 +1000

    First Push

---
+++ src/LexicalAnalyser.java
@@ -0,0 +1,12 @@
+import java.util.Collections;
+import java.util.List;
+
+public class LexicalAnalyser {
+
+	public static List<Token> analyse(String sourceCode) throws LexicalException {
+
+		return Collections.emptyList();
+				
+	}
+
+}
---
+++ src/SyntacticAnalyser.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class SyntacticAnalyser {
+
+	public static ParseTree<Token> parse(List<Token> tokens) throws SyntaxException {
+
+		return new ParseTree<Token>();
+
+	}
+
+}
