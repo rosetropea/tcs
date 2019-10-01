d8628287b6b794fb54dfb0830150639bfa0fec9f
lukeratcliffe <49473471+lukeratcliffe@users.noreply.github.com>
2019-10-01 15:54:59 +1000

    Wrote function notes
    LExical Analyser notes on functions and functionality

--- src/LexicalAnalyser.java
+++ src/LexicalAnalyser.java
@@ -3,10 +3,34 @@ import java.util.List;
 
 public class LexicalAnalyser {
 
-	public static List<Token> analyse(String sourceCode) throws LexicalException {
+    public static List<Token> analyse(String sourceCode) throws LexicalException {
 
-		return Collections.emptyList();
-				
-	}
+        /**
+           Separate into array of strings
+           Split by whitespace and by terminals?
+           Check For (String str | array[])
+           if statment for all terminals
+           if (str = terminal) then (Add that Token.type(str) to List<Token>)
+           else then (str is variable identify ID, Num, String etc (Add to Token.type(variable) to Lis<Token)>))
+           return List<Token>;
+           */
+            return null;
+    }
+    
+    public static void stringCheck(String str){
+       /**
+          function check each character in string
+          While (i = 0, i < str.length, i++){
+           if (str.charAt(i) == + / * - % ( ) { } = & " '
+              
+           }
+           "String A BEcaue "
+           Check if is a variable
+           when runs into a terminal it will split the string and tokenise the variable before the split 
+           and the terminal used in the split. After tokenise continue analysing the string
+          */
+         int number=1+0;
+         
+       }
 
 }
