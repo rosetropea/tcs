19bf11bf0af60dc5c0a17e90bc8159b421986e28
Rosemary <13196225@student.uts.edu.au>
2019-10-03 16:35:30 +1000

    Planning the Syntactic Analyser
    planning comments and some start to code

--- src/SyntacticAnalyser.java
+++ src/SyntacticAnalyser.java
@@ -1,11 +1,35 @@
 import java.util.List;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class SyntacticAnalyser {
 
-	public static ParseTree<Token> parse(List<Token> tokens) throws SyntaxException {
+     Deque<Token> deque = new LinkedList<Token>(); 
+     
+    public ParseTree<Token> parse(List<Token> tokens) throws SyntaxException {
+        
+        //  METHOD PLAN:
+        //  for (Token token : tokens)
+        //  Get token
+        //  Push it to the stack
+        //  Get the next token
+        //  if the next token is valid, push it to the stack
+        //  else throw new SyntaxException
+        //  System.out.print("Passed");
+        
+        for (Token t : tokens) {
+           deque.push(t);
+            if (deque.peekFirst() != null) {
+            deque.peekFirst(); 
+           }
+        }
+        
+        
+        return new ParseTree<Token>();
 
-		return new ParseTree<Token>();
-
-	}
+    }
+    
+    
+    
 
 }
