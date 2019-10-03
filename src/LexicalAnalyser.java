f5a7c3c5dbf17fe250c9d950f6aa1e74c17c2535
lukeratcliffe <49473471+lukeratcliffe@users.noreply.github.com>
2019-10-03 16:24:29 +1000

    Working Split String with Delimiteres
    Title says it all boiiii

--- src/LexicalAnalyser.java
+++ src/LexicalAnalyser.java
@@ -1,11 +1,13 @@
 import java.util.Collections;
 import java.util.List;
+import java.util.Arrays;
 
 public class LexicalAnalyser {
 
     public static List<Token> analyse(String sourceCode) throws LexicalException {
 
         /**
+           Trim white space in array
            Separate into array of strings
            Split by whitespace and by terminals?
            Check For (String str | array[])
@@ -14,6 +16,9 @@ public class LexicalAnalyser {
            else then (str is variable identify ID, Num, String etc (Add to Token.type(variable) to Lis<Token)>))
            return List<Token>;
            */
+
+          String [] array = sourceCode.split("((?<=\\))|(?=\\)))|((?<=\\()|(?=\\())|((?<=\\+)|(?=\\+))|((?<=\\*)|(?=\\*))|((?<=\\/)|(?=\\/))|((?<=\\-)|(?=\\-))|((?<=\\%)|(?=\\%))|((?<=\")|(?=\"))|((?<=\')|(?=\'))((?<=\\})|(?=\\}))|((?<=\\{)|(?=\\{))|((?<=\\&)|(?=\\&))|((?<=\\|)|(?=\\|))|((?<=\\=)|(?=\\=))|((?<=\\!)|(?=\\!))|((?<=\\>)|(?=\\>))|((?<=\\<)|(?=\\<))|((?<=\\;)|(?=\\;))|\\s+");
+          System.out.println(Arrays.toString(array));
             return null;
     }
     
